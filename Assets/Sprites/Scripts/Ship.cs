a8dee8dc0ed72428cb478eb192232623c0b2e1cd
DESKTOP-PO2B61U\cooli <arg17801@uvg.edu.gt>
2018-04-25 09:23:28 -0600

    Proyecto casi terminado falta lo del tiempo

---
+++ Assets/Sprites/Scripts/Ship.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+    Rigidbody2D rb;
+    private float speed = 10f;
+    
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (transform.position.y < 2.5 && transform.position.y > -2.5)
+        {
+            float movX = Input.acceleration.x;
+            rb.transform.Translate(Vector2.right * speed * movX * Time.deltaTime);
+        }
+
+    }
+}
