9657d771f828c3e1a7ed8ffe7aadc29ea881eb5b
diazMafer <31084398+diazMafer@users.noreply.github.com>
2019-03-04 22:12:04 -0600

    Avances Proyecto MAFER
    DISEÑO DE VIDEOJUEGOS

--- Juego correcto/cc3055-lab04-android-mafer-y-andrea-master/Assets/Sprites/Scripts/Ship.cs
+++ Juego correcto/cc3055-lab04-android-mafer-y-andrea-master/Assets/Sprites/Scripts/Ship.cs
@@ -10,13 +10,18 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour {
     //Atributos
+    private Vector2 startTouchPosition, endTouchPosition;
     Rigidbody2D rb;
+    private float force = 700f;
+    private bool jump = false;
     private float speed = 10f;
     
 
 	// Use this for initialization
 	void Start () {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
         rb = GetComponent<Rigidbody2D>();
+
 		
 	}
 
@@ -24,16 +29,44 @@ public class Ship : MonoBehaviour {
     void Update() {
         if (GameController.instance.gameOver == false)
         {
+
                 //Mientras no haya perdido, aun se mueve la nave
                 speed = 10f;
-                float movX = Input.acceleration.x;
-                rb.transform.Translate(Vector2.right * speed * movX * Time.deltaTime);
-       
+                float movY = Input.acceleration.x;
+                rb.transform.Translate(Vector2.right * speed * movY * Time.deltaTime);
+                SwipeCheck();
+                Allowed();
+
         }
     }
+
+   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //Si choca con un collider que esta triggered, pierde
         GameController.instance.gameOver = true;
+    }
+
+    private void SwipeCheck()
+    {
+        //se tienen que guardar las posiciones iniciales y finales del touch
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+            startTouchPosition = Input.GetTouch(0).position;
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+            endTouchPosition = Input.GetTouch(0).position;
+        
+      
+        if (endTouchPosition.y > startTouchPosition.y && rb.velocity.y == 0)
+            jump = true;
+    }
+
+    private void Allowed()
+    {
+        if (jump)
+        {
+            rb.AddForce(Vector2.up * force);
+            jump = false;
+        }
     }
 }
